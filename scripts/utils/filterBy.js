4c56aff441fb3e2b4bce1041ba5389f51fcd1a05
JohanHauteville <johan.hauteville@gmail.com>
2023-03-23 11:58:05 +0100

    Add name changing on filter Button

--- scripts/utils/filterBy.js
+++ scripts/utils/filterBy.js
@@ -1,19 +1,34 @@
 
 const btnFilter = document.querySelector('.btn-filter');
 btnFilter.addEventListener('click',handleFilters);
-// const bdyEvent= document.querySelector('body')
-const liste = document.querySelector('.list-filter')
-liste.addEventListener('click',handleFilters)
-// bdyEvent.addEventListener('click',function(){
-//     if(listOfFilters.style.display === "block"){
-//         listOfFilters.style.display = "none"
-//     }
-// })
+
 const listOfFilters = document.querySelector('.list-filter')
 listOfFilters.style.display = "none"
 
+const allFilters = document.querySelectorAll('.list-filter li')
+allFilters.forEach(element => {
+    element.addEventListener('click',() =>{
+        btnFilter.innerHTML= element.textContent + "<i class=\"fa-solid fa-chevron-down\">"
+        switch(element.textContent){
+            case 'Popularité':
+                console.log('Tri par popularité')
+                break
+            case 'Date':
+                console.log('Tri par Date')
+                break
+            case 'Titre':
+                console.log('Tri par Titre')
+                break
+            default:
+                console.log('Aucun tri');
+                break
+        }
+        handleFilters()
+    })
+});
+
 // state pattern
-export function handleFilters(){
+function handleFilters(){
     if(listOfFilters.style.display === "none"){
         listOfFilters.style.display = "block"
     }else if(listOfFilters.style.display === "block"){
