d811d8efabfc2f05bd20af259a3ab136c05307b9
JohanHauteville <johan.hauteville@gmail.com>
2023-03-24 12:22:40 +0100

    Add ARIA on filter btn

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -9,6 +9,10 @@ const mainElements = document.querySelector("main");
 const btnSend = document.querySelector('.send_button');
 btnSend.addEventListener('click',sendData);
 
+const  focusableElements =
+    'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+    console.log(focusableElements);
+
 export function displayModal() {
     modal.style.display = "flex"; 
     modal.setAttribute("aria-hidden",false)
@@ -28,12 +32,7 @@ export function closeModal() {
 }
 
 
-modal.addEventListener("keydown", function(e){
-    console.log(e.key);
-    if(e.key === "Escape"){
-        closeModal()
-    }
-})
+
 
 export function sendData(){
     // Code d'envoi des données
@@ -41,3 +40,36 @@ export function sendData(){
     //location.reload()
     //event.preventDefault()
 }
+
+
+const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+const focusableContent = modal.querySelectorAll(focusableElements);
+const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+modal.addEventListener('keydown', function(e) {
+    
+    let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+    //Ferme la modale si ECHAP est utilisé
+    if(e.key === "Escape"){
+        closeModal()
+    }
+    // Sort de la boucle si TAB n'est pas utilisé
+    if (!isTabPressed) {
+      return;
+    }
+
+  
+    if (e.shiftKey) { // Si SHIFT + TAB est utilisé
+      if (document.activeElement === firstFocusableElement) {
+        lastFocusableElement.focus(); // Le focus est mis sur le dernier elementde la modale
+        e.preventDefault();
+      }
+    } else { // Si TAB est utilisé...
+      if (document.activeElement === lastFocusableElement) { //... et si en utilisant TAB on arrive au dernier élément de la modale
+        firstFocusableElement.focus(); // Le focus cible alors le premier élément de la modale
+        e.preventDefault();
+      }
+    }
+  });
+  
+ // firstFocusableElement.focus();
--- scripts/utils/filterBy.js
+++ scripts/utils/filterBy.js
@@ -6,32 +6,123 @@ const listOfFilters = document.querySelector('.list-filter')
 listOfFilters.style.display = "none"
 
 const allFilters = document.querySelectorAll('.list-filter li')
-allFilters.forEach(element => {
-    element.addEventListener('click',() =>{
-        btnFilter.innerHTML= element.textContent + "<i class=\"fa-solid fa-chevron-down\">"
-        switch(element.textContent){
-            case 'Popularité':
-                console.log('Tri par popularité')
-                break
-            case 'Date':
-                console.log('Tri par Date')
-                break
-            case 'Titre':
-                console.log('Tri par Titre')
-                break
-            default:
-                console.log('Aucun tri');
-                break
-        }
-        handleFilters()
-    })
-});
+console.log(allFilters);
+// allFilters.forEach(element => {
+//     element.addEventListener('click',() =>{
+//         btnFilter.innerHTML= element.textContent + "<i class=\"fa-solid fa-chevron-down\">"
+//         switch(element.textContent){
+//             case 'Popularité':
+//                 console.log('Tri par popularité')
+//                 break
+//             case 'Date':
+//                 console.log('Tri par Date')
+//                 break
+//             case 'Titre':
+//                 console.log('Tri par Titre')
+//                 break
+//             default:
+//                 console.log('Aucun tri');
+//                 break
+//         }
+//         handleFilters()
+//     })
+// });
 
 // state pattern
 function handleFilters(){
     if(listOfFilters.style.display === "none"){
         listOfFilters.style.display = "block"
+        listOfFilters.focus()
+        btnFilter.setAttribute("aria-expanded",true)
+        //Supprime le "focus" visible
+        allFilters.forEach(element => element.classList.remove('is-selected'))
     }else if(listOfFilters.style.display === "block"){
         listOfFilters.style.display = "none"
+        btnFilter.setAttribute("aria-expanded",false)
+        btnFilter.focus()
+        console.log('fermeture');
     }
+}
+
+listOfFilters.addEventListener("keydown", function(e){
+    console.log(e.key)
+    if(e.key === "Escape"){
+       handleFilters()
+    }
+    else if(e.key === "Enter"){
+        document.activeElement.click()
+    }    
+})
+
+let indice = 0
+listOfFilters.addEventListener("keyup", function(e){
+    if(e.key === "Enter" && indice >=1){
+        handleFilters()
+        indice = 0
+    } else{
+        indice++
+    }
+})
+
+/////  EVENEMENT CLICK  //////////
+listOfFilters.addEventListener('click', event => {
+    const option = event.target.closest('li')
+    console.log(option);
+    if (!option) return
+  
+    // Change la valeur de aria-activedescendant 
+    listOfFilters.setAttribute('aria-activedescendant', option.id)
+    btnFilter.innerHTML= option.textContent + "<i class=\"fa-solid fa-chevron-down\">"
+    handleFilters()
+  
+    // Changement de l'apparence visuelle
+    allFilters.forEach(element => element.classList.remove('is-selected'))
+    option.classList.add('is-selected')
+
+    // Change la valeur de aria-selected
+    allFilters.forEach(element => element.setAttribute("aria-selected",false))
+    option.setAttribute("aria-selected",true)
+  })
+
+/////  EVENEMENT CLAVIER  //////////
+listOfFilters.addEventListener('keydown', event => {
+const { key } = event
+
+if (key === 'ArrowDown' || key === 'ArrowUp'){
+    listOfFilters.addEventListener('keydown' , disableArrowKeys, false);
+    listOfFilters.addEventListener('keyup'   , disableArrowKeys, false);
+    console.log("Down !");
+    const activeElementID = listOfFilters.getAttribute('aria-activedescendant')
+    const activeElement = listOfFilters.querySelector('#' + activeElementID)
+    // ...
+    let selectedOption 
+    if (key === 'ArrowDown') selectedOption = activeElement.nextElementSibling
+    if (key === 'ArrowUp') selectedOption = activeElement.previousElementSibling
+
+    
+    if (selectedOption) {     
+        // Change la valeur de aria-activedescendant
+        listOfFilters.setAttribute('aria-activedescendant', selectedOption.id)
+    
+        // Changement de l'apparence visuelle
+        allFilters.forEach(element => element.classList.remove('is-selected'))
+        selectedOption.classList.add('is-selected')
+
+        // Change la valeur de aria-selected
+        allFilters.forEach(element => element.setAttribute("aria-selected",false))
+        selectedOption.setAttribute("aria-selected",true)
+    }
+}
+
+
+})
+
+
+//function pour desactiver le scroll avec les touches fléchées
+function disableArrowKeys(evt) {
+    (evt.keyCode === 37 ||
+     evt.keyCode === 38 ||
+     evt.keyCode === 39 ||
+     evt.keyCode === 40) &&
+    evt.preventDefault();
 }
