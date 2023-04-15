750c0bc663d252fad758c423bf2d5641b1d47e77
JohanHauteville <johan.hauteville@gmail.com>
2023-04-15 11:55:44 +0200

    Update comments on LightBox.js

--- scripts/utils/lightBox.js
+++ scripts/utils/lightBox.js
@@ -1,23 +1,28 @@
+//*********    CODE DE LA LIGHTBOX   *********//
+
+
 const mainElements = document.querySelector('main')
 const lightBox = document.getElementById('light-box')
 const closeBtn = document.querySelector('.light-box-close')
+const mediaBox = document.querySelector('.media-light-box')
+const nextMediaBtn = document.querySelector('#btn-next-media')
+const previousMediaBtn = document.querySelector('#btn-previous-media')
+let indexNumber = 0
+let arrayOfIndexLength = 0
+const  focusableElements = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
 closeBtn.addEventListener('click', closeLightbox)
 closeBtn.addEventListener('keydown', event=>{
     if(event.key==='Enter'){
         closeLightbox()
     }
 })
-const mediaBox = document.querySelector('.media-light-box')
-const nextMediaBtn = document.querySelector('#btn-next-media')
 nextMediaBtn.addEventListener('click',nextMedia)
-const previousMediaBtn = document.querySelector('#btn-previous-media')
 previousMediaBtn.addEventListener('click',previousMedia)
-let indexNumber = 0
-let arrayOfIndexLength = 0
-const  focusableElements = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
 
 /////// GÈRE LA NAVIGATION CLAVIER DANS LA LIGHTBOX ///////
 lightBox.addEventListener('keydown', event =>{
+    // Dans un premier temps, permet de désactiver le comportement par défaut des touches.
     (event.key === 'ArrowUp' ||
         event.key === 'ArrowDown' ||
         event.key === 'ArrowLeft' ||
@@ -46,7 +51,7 @@ lightBox.addEventListener('keydown', function(e) {
     }
     if (e.shiftKey) { // Si SHIFT + TAB est utilisé
       if (document.activeElement === firstFocusableElement) {
-        lastFocusableElement.focus(); // Le focus est mis sur le dernier elementde la modale
+        lastFocusableElement.focus(); // Le focus est mis sur le dernier element de la modale
         e.preventDefault();
       }
     } else { // Si TAB est utilisé...
@@ -63,19 +68,29 @@ export function showLightbox(mediaId){
     lightBox.style.display = "flex"
     lightBox.setAttribute("aria-hidden",false)
     mainElements.setAttribute("aria-hidden",true)
+
     // Le focus est placé sur le bouton previous
     previousMediaBtn.focus()
+
     // Permet de récupérer la taille maximale du tableau
     const arrayOfDataIndexNumber = document.querySelectorAll('[data-index-number]')
     arrayOfIndexLength = arrayOfDataIndexNumber.length 
 
     const mediaToShow = document.querySelector(`[data-media-id="${mediaId}"]`)
+
+    // récupère l'index du média afficher dans la variable globale indexNumber.
+    // cela nous permettra de l'utiliser dans les fonctions permettant d'afficher le média suivant/précédent
     indexNumber = mediaToShow.getAttribute("data-index-number")
 
+    // Permet de copier le Node présent dans la grille de médias
     const copyMedia = mediaToShow.cloneNode()
     const titleElement = document.createElement("p")
+
+    // récupère le titre de la copie du Node
     const titleFromCopy = copyMedia.getAttribute("alt")
     titleElement.textContent = titleFromCopy
+
+    // Permet à l'utilisateur d'avoir le controle sur la vidéo
     copyMedia.setAttribute("controls","")
 
     mediaBox.appendChild(copyMedia)
@@ -84,6 +99,7 @@ export function showLightbox(mediaId){
 
 /////// FONCTION DE FERMETURE DE LA LIGHTBOX ///////
 function closeLightbox(){
+    // Permet lors de la fermeture de la lightbox de placer le focus sur le dernier élément affiché
     const mediaToReturnFocus = document.querySelector(`[data-index-number="${indexNumber}"]`)
     mediaToReturnFocus.focus()
     indexNumber=0
@@ -95,6 +111,7 @@ function closeLightbox(){
 
 /////// FONCTION DU BOUTON NEXT DE LA LIGHTBOX ///////
 function nextMedia(){
+    // permet de bouclier sur le premier média si le dernier a été atteint
     if(indexNumber>=arrayOfIndexLength-1){
         indexNumber=0
     } else{
@@ -115,6 +132,7 @@ function nextMedia(){
 
 /////// FONCTION DU BOUTON PREVIOUS DE LA LIGHTBOX ///////
 function previousMedia(){
+    // permet de bouclier sur le dernier média si le premier a été atteint
     if(indexNumber==0){
         indexNumber=arrayOfIndexLength-1
     } else{
