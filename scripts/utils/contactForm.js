0b7621dc5e8d1bea6938d242c97b09e2332a9f7d
JohanHauteville <johan.hauteville@gmail.com>
2023-04-15 10:44:23 +0200

    Add comments on files

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,7 +1,5 @@
 const btn = document.querySelector('.contact_button')
-btn.addEventListener('click',displayModal)
 const btnClose = document.querySelector('.contact-close')
-btnClose.addEventListener('click',closeModal)
 const titleModal = document.getElementById('modal-title');
 const modal = document.getElementById("contact_modal");
 const mainElements = document.querySelector("main");
@@ -9,37 +7,41 @@ const inputOfFirst = document.getElementById('first')
 const inputOfLast = document.getElementById('last')
 const inputOfMail = document.getElementById('mail')
 const inputOfMessage = document.getElementById('message')
-
-
 const btnSend = document.querySelector('.send_button');
-btnSend.addEventListener('click',sendData);
 
-const  focusableElements =
-    'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
 
-export function displayModal() {
-    modal.style.display = "flex"; 
-    modal.setAttribute("aria-hidden",false)
-    modal.setAttribute("aria-modal",true)
-    mainElements.setAttribute("aria-hidden",true)
-    titleModal.focus()
-    
+btn.addEventListener('click', displayModal)       // Affiche la modale au click du "contacter moi"
+btnClose.addEventListener('click', closeModal)    // Ferme la modale au click du "X"
+btnSend.addEventListener('click', sendData);      // Affiche les données envoyées par le formulaire au click du bouton
+
+// On définit tous les éléments qui peuvent récupérer le focus dans notre modale
+const focusableElements = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
+// AFFICHE LA MODALE
+function displayModal() {
+  modal.style.display = "flex";
+  modal.setAttribute("aria-hidden", false)
+  modal.setAttribute("aria-modal", true)
+  mainElements.setAttribute("aria-hidden", true)
+  // Focus mis sur le titre de la modale
+  titleModal.focus()
 }
 
-export function closeModal() {
-    modal.style.display = "none";
-    modal.setAttribute("aria-hidden",true)
-    mainElements.setAttribute("aria-hidden",false)
-    btn.focus()
+// FERME LA MODALE
+function closeModal() {
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", true)
+  mainElements.setAttribute("aria-hidden", false)
+  btn.focus()
 }
 
-export function sendData(){
- if(
-   checkTextarea() &
-   checkString("first") &
-   checkString("last") &
-   checkEmail()
- ){
+export function sendData() {
+  if (
+    checkTextarea() &
+    checkString("first") &
+    checkString("last") &
+    checkEmail()
+  ) {
 
     console.log(`Envoi du formulaire:
     Prénom: ${inputOfFirst.value}
@@ -52,49 +54,47 @@ export function sendData(){
     inputOfLast.value = ""
     inputOfMail.value = ""
     inputOfMessage.value = ""
- } 
-    event.preventDefault()
-    
+  }
+  event.preventDefault()
+
 }
 
 const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
 const focusableContent = modal.querySelectorAll(focusableElements);
 const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
 
-modal.addEventListener('keydown', function(e) {
-    
-    let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
-    //Ferme la modale si ECHAP est utilisé
-    if(e.key === "Escape"){
-        closeModal()
-    }
-    // Sort de la boucle si TAB n'est pas utilisé
-    if (!isTabPressed) {
-      return;
-    }
+modal.addEventListener('keydown', function (e) {
 
-  
-    if (e.shiftKey) { // Si SHIFT + TAB est utilisé
-      if (document.activeElement === firstFocusableElement) {
-        lastFocusableElement.focus(); // Le focus est mis sur le dernier elementde la modale
-        e.preventDefault();
-      }
-    } else { // Si TAB est utilisé...
-      if (document.activeElement === lastFocusableElement) { //... et si en utilisant TAB on arrive au dernier élément de la modale
-        firstFocusableElement.focus(); // Le focus cible alors le premier élément de la modale
-        e.preventDefault();
-      }
+  let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+  //Ferme la modale si ECHAP est utilisé
+  if (e.key === "Escape") {
+    closeModal()
+  }
+  // Sort de la boucle si TAB n'est pas utilisé
+  if (!isTabPressed) {
+    return;
+  }
+  if (e.shiftKey) { // Si SHIFT + TAB est utilisé
+    if (document.activeElement === firstFocusableElement) {
+      lastFocusableElement.focus(); // Le focus est mis sur le dernier elementde la modale
+      e.preventDefault();
     }
+  } else { // Si TAB est utilisé...
+    if (document.activeElement === lastFocusableElement) { //... et si en utilisant TAB on arrive au dernier élément de la modale
+      firstFocusableElement.focus(); // Le focus cible alors le premier élément de la modale
+      e.preventDefault();
+    }
+  }
 });
 
-inputOfFirst.addEventListener('focusout',()=>{
+inputOfFirst.addEventListener('focusout', () => {
   checkString("first")
 })
-inputOfLast.addEventListener('focusout',()=>{
+inputOfLast.addEventListener('focusout', () => {
   checkString("last")
 })
 inputOfMail.addEventListener('focusout', checkEmail)
-inputOfMessage.addEventListener('focusout',checkTextarea)
+inputOfMessage.addEventListener('focusout', checkTextarea)
 
 
 
@@ -102,11 +102,11 @@ inputOfMessage.addEventListener('focusout',checkTextarea)
 function affichageErreur(inputId, text) {
   if (text === "DEL") {    // Si la chaine est vide alors on supprime l'affichage de l'erreur
     document.getElementById(inputId).removeAttribute("data-error-visible");
-    document.getElementById(inputId).setAttribute("data-valid-visible","");
+    document.getElementById(inputId).setAttribute("data-valid-visible", "");
 
 
-  } else if(text === "ADD"){   // Sinon on affiche l'erreur avece le texte entré
-    document.getElementById(inputId).setAttribute("data-error-visible","");
+  } else if (text === "ADD") {   // Sinon on affiche l'erreur avece le texte entré
+    document.getElementById(inputId).setAttribute("data-error-visible", "");
     document.getElementById(inputId).removeAttribute("data-valid-visible");
 
   }
@@ -116,14 +116,14 @@ function affichageErreur(inputId, text) {
 function checkEmail() {
   const regex = /^([a-zA-Z0-9-.]+)(@)([a-zA-Z-]+)\.([a-zA-Z]{2,})$/mg;  // Regex de test
   if (/@/.test(inputOfMail.value) === false) {                    // Test de la présence de '@'
-    affichageErreur('mail',"ADD");
+    affichageErreur('mail', "ADD");
   } else if (/(@)([a-zA-Z-]+)\.([a-zA-Z]{2,})$/mg.test(inputOfMail.value) === false) {     // Test de la présence du nom de domaine complet
-    affichageErreur('mail',"ADD");
+    affichageErreur('mail', "ADD");
   } else if (regex.test(inputOfMail.value)) {           // Vérification du reste de l'adresse email (avant le '@')
-    affichageErreur('mail',"DEL");
+    affichageErreur('mail', "DEL");
     return true;
   } else {
-    affichageErreur('mail',"ADD");
+    affichageErreur('mail', "ADD");
   }
   return false;
 }
@@ -133,23 +133,23 @@ function checkString(elementID) {
   const regex = /^[a-zA-Z-\s]{2,}$/mg;
 
   const valueOfInput = document.getElementById(elementID).value;
-  if(regex.test(valueOfInput)){
-    affichageErreur(elementID,"DEL")
+  if (regex.test(valueOfInput)) {
+    affichageErreur(elementID, "DEL")
     return true
-  }else{
-    affichageErreur(elementID,"ADD")
+  } else {
+    affichageErreur(elementID, "ADD")
     return false
   }
 }
 
-function checkTextarea(){
+function checkTextarea() {
   const regex = /\w[a-zA-Z0-9-.\s]{2,}/mg;
   const valueOfTextarea = document.getElementById('message').value
-  if(regex.test(valueOfTextarea)){
-    affichageErreur("message","DEL")
+  if (regex.test(valueOfTextarea)) {
+    affichageErreur("message", "DEL")
     return true
-  }else{
-    affichageErreur("message","ADD")
+  } else {
+    affichageErreur("message", "ADD")
     return false
   }
 }
